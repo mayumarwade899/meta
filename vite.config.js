f275df2de83b6859ea1096fc591a01ec7c52510e
Mayur Marwade <mayumarwade899@gmail.com>
2024-12-26 14:26:01 +0530

    added deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vite-deploy/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
